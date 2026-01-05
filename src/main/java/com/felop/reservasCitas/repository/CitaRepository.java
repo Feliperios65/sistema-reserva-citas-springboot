20a01bb0ebc0ff57b02f1c547685e5ca80f3f88d
Felipe Rios <feliperios665@gmail.com>
2026-01-04 20:43:10 -0500

    anotaciones en repository corregidas

--- src/main/java/com/felop/reservasCitas/repository/CitaRepository.java
+++ src/main/java/com/felop/reservasCitas/repository/CitaRepository.java
@@ -7,12 +7,14 @@ import com.felop.reservasCitas.model.EstadoCita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CitaRepository extends JpaRepository<Cita, Long> {
 
     //busca cita por su codigo unico de confirmacion
